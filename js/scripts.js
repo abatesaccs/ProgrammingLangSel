6c284f2a54071f29ca424d63c621cac112318a02
Andrew <abatesaccs@gmail.com>
2019-10-26 15:42:57 -0700

    add forms for each select option

--- js/scripts.js
+++ js/scripts.js
@@ -4,22 +4,42 @@ $(document).ready(function(){
 
   event.preventDefault();
   });
-  var lowVsHigh = $('#question3').val();
+  $("form#questionform").submit(function(event) {
+  var lowVsHigh = $('#question5').val();
   var logicVsDesign = $('#question4').val();
-  var catVsDog = $('#question5').val();
 
-  if (true) {
+  if (lowVsHigh === true) {
+    $(".pythonpanel").show();
+    $(".htmlpanel").show();
+  } else if (logicVsDesign === false) {
+    $(".htmlpanel").show();
+  } else if (lowVsHigh === false) {
 
   }
 
-  switch(lowVsHigh, logicVsDesign, catVsDog) {
-  case lowVsHigh ^ logicVsDesign ^ catVsDog:
-    // code block
-    break;
-  case y:
-    // code block
-    break;
-  default:
-    // code block
-  }
+  // $(function() {
+  //      $('#question4').change(function(){
+  //          $('.htmlpanel .pythonpanel .assemblypanel').hide();
+  //          $('#' + $(this).val()).show();
+  //      });
+  //  });
+
+
+
+  // switch(true) {
+  // case (logicVsDesign === false):
+  //   $(".htmlpanel").show()
+  //   break;
+  // case (lowVsHigh === true):
+  //   $(".pythonpanel").show()
+  //   break;
+  // case (lowVsHigh === false):
+  //   $(".assemblypanel").show()
+  //   break;
+  // default:
+  //   // code block
+  // }
+  event.preventDefault();
+  });
+  $(".pythonpanel").show();
 });
