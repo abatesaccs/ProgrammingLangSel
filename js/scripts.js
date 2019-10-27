8c756ef9a41f372e290554e9137b7f373eec6132
Andrew <abatesaccs@gmail.com>
2019-10-26 18:00:34 -0700

    add processing and seperate html and css

--- js/scripts.js
+++ js/scripts.js
@@ -1,66 +1,62 @@
 $(document).ready(function(){
   $("form#nameform").submit(function(event) {
   var someInput = $("input#nameinput").val();
-
   event.preventDefault();
   });
-  $("form#questionform").submit(function(event) {
-  var lowVsHigh = $('#question5').val();
-  var logicVsDesign = $('#question4').val();
-
-  if (lowVsHigh === "high") {
-    $(".pythonpanel").show();
-    $(".htmlpanel").show();
-  } else if (logicVsDesign === false) {
-    $(".htmlpanel").show();
-  } else if (lowVsHigh === "low") {
-
-  }
-  event.preventDefault();
-  });
-  // $("button.HorL").click(function(){
-  //   var value = $('#question5').val();
-  //   if (value === "high") {
-  //     $(".htmlpanel").toggle();
-  //     $(".pythonpanel").toggle();
-  //   }else {
-  //     $(".assemblypanel").toggle();
-  //   }
-  // });
-  //
-  // $("button.LorD").click(function(){
-  //   var value = $('#question4').val();
-  //   if (value === "logic") {
-  //     $(".assemblypanel").show();
-  //     $(".pythonpanel").show();
-  //
-  //   }else {
-  //     $(".htmlpanel").show();
-  //   }
-  // });
 
   $("button.submit").click(function(){
+    var q3value = $('#question3').val();
     var q4value = $('#question4').val();
     var q5value = $('#question5').val();
     if (q4value === "logic" && q5value === "high") {
       $(".pythonpanel").show();
+      $(".htmlpanel").hide();
+      $(".assemblypanel").hide();
+      $(".processingpanel").hide();
     } else if (q4value === "logic" && q5value === "low") {
       $(".assemblypanel").show();
-    } else if (q4value === "design") {
+      $(".htmlpanel").hide();
+      $(".pythonpanel").hide();
+      $(".processingpanel").hide();
+    } else if (q4value === "design" && q3value === "web") {
       $(".htmlpanel").show();
+      $(".pythonpanel").hide();
+      $(".assemblypanel").hide();
+      $(".processingpanel").hide();
+    } else if (q4value === "design" && q3value === "software") {
+      $(".processingpanel").show();
+      $(".assemblypanel").hide();
+      $(".pythonpanel").hide();
+      $(".htmlpanel").hide();
     } else {
+      $(".processingpanel").hide();
       $(".assemblypanel").hide();
       $(".pythonpanel").hide();
       $(".htmlpanel").hide();
     }
-
-    // if (value === "logic") {
-    //   $(".assemblypanel").show();
-    //   $(".pythonpanel").show();
-    //
-    // }else {
-    //   $(".htmlpanel").show();
-    // }
   });
+  $("button.submit").click(function(){
+    var q3value = $('#question3').val();
+    var q4value = $('#question4').val();
+    var q5value = $('#question5').val();
+    if (q3value === "web") {
 
+      if (q4value === "design" && q5value === "high") {
+        $(".csspanel").show();
+        $(".software").hide();
+        $(".htmlpanel").hide();
+      } else if (q4value === "logic" && q5value === "high") {
+        $(".htmlpanel").show();
+        $(".csspanel").hide();
+        $(".software").hide();
+      } else if ((q4value === "design" || q4value === "logic") && q5value === "low") {
+        $(".software").hide();
+        $(".web").hide();
+      }
+
+    } else if (true) {
+
+    }
+
+  });
 });
